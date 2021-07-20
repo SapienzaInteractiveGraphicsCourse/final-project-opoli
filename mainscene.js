e17af3f83e04091df1aab3b0be868c11e068844c
6ninjak <colizzicoin.1794538@studenti.uniroma1.it>
2021-07-20 15:48:50 +0200

    started conversion

--- mainscene.js
+++ mainscene.js
@@ -9,11 +9,54 @@ const {
   PointerLock,
   PointerDrag
 } = ENABLE3D
+import TWEEN, { Easing, Tween } from './libs/tween.esm.js';
+import { PointerLockControls } from './three.js-master/examples/jsm/controls/PointerLockControls.js';
 
-/**
- * Is touch device?
- */
+// /**
+//  * Is touch device?
+//  */
 const isTouchDevice = 'ontouchstart' in window
+
+class ThirdPersonCamera {
+	constructor(params) {
+		this._params = params;
+		this._camera = params.camera;
+
+		this._currentPosition = new THREE.Vector3();
+		this._currentLookAt = new THREE.Vector3();
+	}
+
+	_CalculateOffset(theta, phi) {
+		const radius = this._params.radius;
+		const offset = new THREE.Vector3(Math.sin(theta) * Math.cos(phi) * radius, Math.sin(phi) * radius, Math.cos(phi) * Math.cos(theta) * radius);
+		offset.applyEuler(this._params.target.rotation);
+		offset.add(this._params.target.position);
+		return offset;
+	}
+
+	_CalculateLookAt() {
+		const lookAt = new THREE.Vector3(0, 0, 0);
+		lookAt.applyEuler(this._params.target.rotation);
+		lookAt.add(this._params.target.position);
+		return lookAt;
+	}
+	SetTarget(target) {
+		this._params.target = target;
+	}
+
+	Update(timePassed, theta, phi) {
+		const offset = this._CalculateOffset(Math.PI - theta, phi);
+		const lookAt = this._CalculateLookAt();
+
+		const interpolate = 1.0 - Math.pow(0.0001, timePassed);
+
+		this._currentPosition.lerp(offset, interpolate);
+		this._currentLookAt.lerp(lookAt, interpolate);
+
+		this._camera.position.copy(this._currentPosition);
+		this._camera.lookAt(this._currentLookAt);
+	}
+}
 
 class MainScene extends Scene3D {
   constructor() {
@@ -28,6 +71,53 @@ class MainScene extends Scene3D {
 
     this.moveTop = 0
     this.moveRight = 0
+    // camera
+	{
+		this.camera = new THREE.PerspectiveCamera(60, this.camera.aspect, 0.1, 1000);
+
+		this.radius = 0.6;
+		this.theta = 0;
+		this.phi = Math.PI / 6;
+
+		// window.addEventListener('resize', () => {
+		// 	width = window.innerWidth;
+		// 	height = window.innerHeight;
+		// 	camera.aspect = width / height;
+		// 	camera.updateProjectionMatrix();
+		// 	renderer.setSize(width, height);
+		// 	max_mousemove = Math.pow(1580, Math.min(1 / camera.aspect, camera.aspect));
+		// }, false);
+
+		this.controls = new PointerLockControls(this.camera, this.canvas);
+
+		this.thirdPersonCamera = new ThirdPersonCamera({
+			camera: this.camera,
+			target: null,
+			radius: this.radius
+		})
+
+		document.addEventListener('click', () => {
+			this.controls.lock();
+		})
+		var max_mousemove = Math.pow(1580, 1 / this.camera.aspect);;
+		document.addEventListener('mousemove', (event) => {
+			if (this.controls.isLocked) {
+				var dx = Math.max(-max_mousemove, Math.min(event.movementX, max_mousemove)) * 0.001;
+				var dy = Math.max(-max_mousemove, Math.min(event.movementY, max_mousemove)) * 0.001;
+				// console.log(event.movementX, event.movementY, camera.aspect)
+
+				this.theta += dx * Math.PI / 2;
+				this.phi += dy * Math.PI / 2;
+				if (Math.abs(phi) > Math.PI / 2) {
+					phi = phi > 0 ? Math.PI / 2 - 0.001 : -Math.PI / 2 + 0.001;
+				}
+				this.phi -= Math.abs(phi) > 2 * Math.PI ? phi < 0 ? -2 * Math.PI : 2 * Math.PI : 0;
+				this.theta -= Math.abs(theta) > 2 * Math.PI ? theta < 0 ? -2 * Math.PI : 2 * Math.PI : 0;
+
+			}
+		})
+
+	}
   }
 
   async preload() {
@@ -36,14 +126,14 @@ class MainScene extends Scene3D {
      * https://sketchfab.com/3d-models/medieval-fantasy-book-06d5a80a04fc4c5ab552759e9a97d91a
      * Attribution 4.0 International (CC BY 4.0)
      */
-    const city = this.load.preload('book', './models/city.fbx')
+    const city = this.load.preload('city', './models/scene.gltf')
 
     /**
      * box_man.glb by Jan BlÃ¡ha
      * https://github.com/swift502/Sketchbook
      * CC-0 license 2018
      */
-    const drone = this.load.preload('man', './models/box_man.glb')
+    const drone = this.load.preload('drone', './models/box_man.glb')
 
     await Promise.all([city, drone])
   }
@@ -59,28 +149,30 @@ class MainScene extends Scene3D {
 
     // this.physics.debug.enable()
 
-    const addBook = async () => {
-      const object = await this.load.fbx('book')
+    const addCity = async () => {
+      const object = await this.load.gltf('city')
       const scene = object.scenes[0]
 
-      const book = new ExtendedObject3D()
-      book.name = 'scene'
-      book.add(scene)
-      this.add.existing(book)
+      const city = new ExtendedObject3D()
+      city.name = 'scene'
+      city.add(scene)
+      city.scale.set(10,10,10)
+      city.position.set(100,-5,500)
+      this.add.existing(city)
 
       // add animations
       // sadly only the flags animations works
       object.animations.forEach((anim, i) => {
-        book.mixer = this.animationMixers.create(book)
+        city.mixer = this.animationMixers.create(city)
         // overwrite the action to be an array of actions
-        book.action = []
-        book.action[i] = book.mixer.clipAction(anim)
-        book.action[i].play()
+        city.action = []
+        city.action[i] = city.mixer.clipAction(anim)
+        city.action[i].play()
       })
 
-      book.traverse(child => {
+      city.traverse(child => {
         if (child.isMesh) {
-          child.castShadow = child.receiveShadow = false
+          child.castShadow = child.receiveShadow = true;
           child.material.metalness = 0
           child.material.roughness = 1
 
@@ -97,20 +189,20 @@ class MainScene extends Scene3D {
         }
       })
     }
-    const addMan = async () => {
-      const object = await this.load.gltf('man')
-      const man = object.scene.children[0]
+    const addDrone = async () => {
+      const object = await this.load.gltf('drone')
+      const drone = object.scene.children[0]
 
-      this.man = new ExtendedObject3D()
-      this.man.name = 'man'
-      this.man.rotateY(Math.PI + 0.1) // a hack
-      this.man.add(man)
-      this.man.rotation.set(0, Math.PI * 1.5, 0)
-      this.man.position.set(35, 0, 0)
+      this.drone = new ExtendedObject3D()
+      this.drone.name = 'man'
+      this.drone.rotateY(Math.PI + 0.1) // a hack
+      this.drone.add(drone)
+      this.drone.rotation.set(0, Math.PI * 1.5, 0)
+      this.drone.position.set(35, 0, 0)
       // add shadow
-      this.man.traverse(child => {
+      this.drone.traverse(child => {
         if (child.isMesh) {
-          child.castShadow = child.receiveShadow = false
+          child.castShadow = child.receiveShadow = true;
           // https://discourse.threejs.org/t/cant-export-material-from-blender-gltf/12258
           child.material.roughness = 1
           child.material.metalness = 0
@@ -121,36 +213,36 @@ class MainScene extends Scene3D {
        * Animations
        */
       // ad the box man's animation mixer to the animationMixers array (for auto updates)
-      this.animationMixers.add(this.man.animation.mixer)
+      this.animationMixers.add(this.drone.animation.mixer)
 
       object.animations.forEach(animation => {
         if (animation.name) {
-          this.man.animation.add(animation.name, animation)
+          this.drone.animation.add(animation.name, animation)
         }
       })
-      this.man.animation.play('idle')
+      this.drone.animation.play('idle')
 
       /**
        * Add the player to the scene with a body
        */
-      this.add.existing(this.man)
-      this.physics.add.existing(this.man, {
+      this.add.existing(this.drone)
+      this.physics.add.existing(this.drone, {
         shape: 'sphere',
         radius: 0.25,
         width: 0.5,
         offset: { y: -0.25 }
       })
-      this.man.body.setFriction(0.8)
-      this.man.body.setAngularFactor(0, 0, 0)
+      this.drone.body.setFriction(0.8)
+      this.drone.body.setAngularFactor(0, 0, 0)
 
       // https://docs.panda3d.org/1.10/python/programming/physics/bullet/ccd
-      this.man.body.setCcdMotionThreshold(1e-7)
-      this.man.body.setCcdSweptSphereRadius(0.25)
+      this.drone.body.setCcdMotionThreshold(1e-7)
+      this.drone.body.setCcdSweptSphereRadius(0.25)
 
       /**
        * Add 3rd Person Controls
        */
-      this.controls = new ThirdPersonControls(this.camera, this.man, {
+      this.controls = new ThirdPersonControls(this.camera, this.drone, {
         offset: new THREE.Vector3(0, 1, 0),
         targetRadius: 3
       })
@@ -172,8 +264,8 @@ class MainScene extends Scene3D {
       }
     }
 
-    addBook()
-    addMan()
+    addCity()
+    addDrone()
 
     /**
      * Add Keys
@@ -205,34 +297,34 @@ class MainScene extends Scene3D {
     document.addEventListener('keydown', e => press(e, true))
     document.addEventListener('keyup', e => press(e, false))
 
-    /**
-     * Add joystick
-     */
-    if (isTouchDevice) {
-      const joystick = new JoyStick()
-      const axis = joystick.add.axis({
-        styles: { left: 20, bottom: 175, size: 100 }
-      })
-      axis.onMove(event => {
-        /**
-         * Update Camera
-         */
-        const { top, right } = event
-        this.moveTop = top
-        this.moveRight = right
-      })
-      const buttonA = joystick.add.button({
-        letter: 'A',
-        styles: { right: 20, bottom: 250, size: 80 }
-      })
-      buttonA.onClick(() => this.jump())
-      const buttonB = joystick.add.button({
-        letter: 'B',
-        styles: { right: 95, bottom: 175, size: 80 }
-      })
-      buttonB.onClick(() => (this.move = true))
-      buttonB.onRelease(() => (this.move = false))
-    }
+    // /**
+    //  * Add joystick
+    //  */
+    // if (isTouchDevice) {
+    //   const joystick = new JoyStick()
+    //   const axis = joystick.add.axis({
+    //     styles: { left: 20, bottom: 175, size: 100 }
+    //   })
+    //   axis.onMove(event => {
+    //     /**
+    //      * Update Camera
+    //      */
+    //     const { top, right } = event
+    //     this.moveTop = top
+    //     this.moveRight = right
+    //   })
+    //   const buttonA = joystick.add.button({
+    //     letter: 'A',
+    //     styles: { right: 20, bottom: 250, size: 80 }
+    //   })
+    //   buttonA.onClick(() => this.jump())
+    //   const buttonB = joystick.add.button({
+    //     letter: 'B',
+    //     styles: { right: 95, bottom: 175, size: 80 }
+    //   })
+    //   buttonB.onClick(() => (this.move = true))
+    //   buttonB.onRelease(() => (this.move = false))
+    // }
 
     setTimeout(() => {
       const placeholder = document.getElementById('welcome-game-placeholder')
@@ -241,18 +333,18 @@ class MainScene extends Scene3D {
   }
 
   jump() {
-    if (!this.man || !this.canJump) return
+    if (!this.drone || !this.canJump) return
     this.canJump = false
-    this.man.animation.play('jump_running', 500, false)
+    this.drone.animation.play('jump_running', 500, false)
     setTimeout(() => {
       this.canJump = true
-      this.man.animation.play('idle', 500)
+      this.drone.animation.play('idle', 500)
     }, 500)
-    this.man.body.applyForceY(6)
+    this.drone.body.applyForceY(6)
   }
 
   update(time, delta) {
-    if (this.man && this.man.body) {
+    if (this.drone && this.drone.body) {
       /**
        * Update Controls
        */
@@ -266,9 +358,9 @@ class MainScene extends Scene3D {
 
       const rotation = this.camera.getWorldDirection(v3)
       const theta = Math.atan2(rotation.x, rotation.z)
-      const rotationMan = this.man.getWorldDirection(v3)
+      const rotationMan = this.drone.getWorldDirection(v3)
       const thetaMan = Math.atan2(rotationMan.x, rotationMan.z)
-      this.man.body.setAngularVelocityY(0)
+      this.drone.body.setAngularVelocityY(0)
 
       const l = Math.abs(theta - thetaMan)
       let rotationSpeed = isTouchDevice ? 2 : 4
@@ -277,22 +369,22 @@ class MainScene extends Scene3D {
       if (l > d) {
         if (l > Math.PI - d) rotationSpeed *= -1
         if (theta < thetaMan) rotationSpeed *= -1
-        this.man.body.setAngularVelocityY(rotationSpeed)
+        this.drone.body.setAngularVelocityY(rotationSpeed)
       }
 
       /**
        * Player Move
        */
       if (this.keys.w.isDown || this.move) {
-        if (this.man.animation.current === 'idle' && this.canJump) this.man.animation.play('run')
+        if (this.drone.animation.current === 'idle' && this.canJump) this.drone.animation.play('run')
 
         const x = Math.sin(theta) * speed,
-          y = this.man.body.velocity.y,
+          y = this.drone.body.velocity.y,
           z = Math.cos(theta) * speed
 
-        this.man.body.setVelocity(x, y, z)
+        this.drone.body.setVelocity(x, y, z)
       } else {
-        if (this.man.animation.current === 'run' && this.canJump) this.man.animation.play('idle')
+        if (this.drone.animation.current === 'run' && this.canJump) this.drone.animation.play('idle')
       }
 
       /**
@@ -315,18 +407,18 @@ window.addEventListener('load', () => {
     project.canvas.style.marginTop = '0px !important'
 
     const resize = () => {
-      const newWidth = window.innerWidth
-      const newHeight = (HEIGHT / WIDTH) * newWidth
+      const newWidth = window.innerWidth;
+      const newHeight = window.innerHeight;
 
-      destination.style.width = `${newWidth}px`
-      destination.style.height = `${newHeight}px`
+      destination.style.width = `${newWidth}px`;
+      destination.style.height = `${newHeight}px1`;
 
-      project.renderer.setSize(newWidth, newHeight)
-      project.camera.aspect = newWidth / newHeight
-      project.camera.updateProjectionMatrix()
+      project.renderer.setSize(newWidth, newHeight);
+      project.camera.aspect = newWidth / newHeight;
+      project.camera.updateProjectionMatrix();
     }
 
-    window.onresize = resize
-    resize()
+    window.onresize = resize;
+    resize();
   })
 })
